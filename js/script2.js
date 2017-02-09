204794debc5febe74faded6ec316d4a85cb3ec50
William <william.legal03@gmail.com>
2017-02-09 11:29:41 +0100

    contenu laFabrik

---
+++ js/script2.js
@@ -0,0 +1,93 @@
+//SLIDE PRESSE //
+$(document).ready(function(){
+    $("#logoHamburger").click(function(){
+        $(".presse").toggle("slow");
+    });
+});
+
+$('#une1').hover(function(){
+    $(this).fadeTo('slow', 0.70);
+  },function(){
+    $(this).fadeTo('slow', 1);
+});
+$('#une3').hover(function(){
+    $(this).fadeTo('slow', 0.70);
+  },function(){
+    $(this).fadeTo('slow', 1);
+});
+$('#une2').hover(function(){
+    $(this).stop().fadeTo('slow', 0.70);
+  },function(){
+    $(this).stop().fadeTo('slow', 1);
+});
+
+//Le JS pour défilement 1 partenaire par 1 partenaire, cherche pas a comprendre non plus, ca marche c cool
+
+var carousels = $('.carousel');
+carousels.each(function() {
+  var $obj = $(this);
+  var $inner = $obj.find('.carousel-inner');
+  
+  var id = 'uuid' + new Date().getTime();
+  $obj.addClass(id);
+
+  if ($obj.data('shift') === 1) {
+    var items = $obj.find('.item > [class*="col-"]'),
+        visibleCnt = $obj.find('.item:first [class*="col-"]').length,
+        wrapper = "";
+    
+    // build styles
+    var rule_base = '.carousel.' + id + ' .carousel-inner > .item',
+        styles = $('<style></style>'),
+        rules = [];
+        rules[0] = rule_base + '.next {left: ' + (100 / visibleCnt) + '%; transform: none;}';
+        rules[1] = rule_base + '.active {left: 0;}';
+        rules[2] = rule_base + '.active.left {left: -' + (100 / visibleCnt) + '%; transform: none;}';
+        rules[3] = rule_base + '.next.left {left: 0;}';
+        rules[4] = rule_base + '.active.right {left: ' + (100 / visibleCnt) + '%; transform: none;}';
+        rules[5] = rule_base + '.prev.right {left: 0;}';
+        rules[6] = rule_base + '.prev {left: -' + (100 / visibleCnt) + '%; transform: none;}';
+    for (var i = 0; i < rules.length; i++) {
+      styles.append(rules[i]);
+    }
+    $obj.prepend(styles);
+
+    // rebuild items
+    for (var i = 0; i < $(items).length; i++) {
+      var $item = $(items[i]);
+      var parent = $item.parent();
+      if (parent.hasClass('item')) {
+        if (!wrapper.length) {
+          wrapper = parent.clone().removeClass('active').html('');
+        }
+        $item.unwrap();
+      }
+      
+      var itemGroup = [$item];
+      for (var x = 1; x < visibleCnt; x++) {
+        var a = i + x;
+        var next = $(items[a]);
+        if (!next.length) {
+          next = $(items[(a - $(items).length)]);
+        }
+        itemGroup[x] = next.clone();
+      }
+      var newSet = wrapper.clone().html(itemGroup);
+      if (i == 0) {
+        newSet.addClass('active');
+      }
+      newSet.appendTo($inner);
+    }
+  }
+});
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36251023-1']);
+  _gaq.push(['_setDomainName', 'jqueryscript.net']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
