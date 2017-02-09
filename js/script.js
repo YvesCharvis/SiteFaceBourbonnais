ffc8cf318da85ce6851310e80eeef76df01c161f
Eddy Huleu <eddy.huleu@live.fr>
2017-02-09 09:29:54 +0100

    maj

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ carousels.each(function() {
   var $obj = $(this);
   var $inner = $obj.find('.carousel-inner');
   
-  var id = 'uuid' + new Date().getTime();
+  var id = 'uuid' + new Date().getTime()  ;
   $obj.addClass(id);
 
   if ($obj.data('shift') === 1) {
@@ -58,13 +58,4 @@ carousels.each(function() {
   }
 });
 
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-36251023-1']);
-  _gaq.push(['_setDomainName', 'jqueryscript.net']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
+ 
